40e884024e6d5e6105427339c2f759095068b903
yousuffaraz006 <yousuffaraz006@gmail.com>
2025-01-25 14:21:58 +0530

    Setup File Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+  name="Medical Chatbot",
+  version="0.0.1",
+  author="Yousuf Faraz",
+  author_email="yisuonline@gmail.com",
+  packages=find_packages(),
+  install_requires=[],
+)
